3e7936a69c9b679afa557732ab2188cc56a2e54e
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-04-18 20:09:53 -0300

    css pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,15 +7,25 @@ function Login() {
   const passwordLength = 6;
 
   return (
-    <div>
-      <form>
+    <div className="loginInputs">
+      <form className="formLogin">
+        <h1>Login</h1>
+        <p>Digite seus dados para acessar.</p>
+        <label htmlFor="email">
+          Digite seu email:
+        </label>
         <input
+          className="emailInput"
           data-testid="email-input"
           type="email"
           value={ email }
           onChange={ ({ target }) => setEmail(target.value) }
         />
+        <label htmlFor="password">
+          Digite sua senha:
+        </label>
         <input
+          className="senhaInput"
           data-testid="password-input"
           type="password"
           value={ password }
@@ -24,6 +34,7 @@ function Login() {
 
         <button
           type="button"
+          className="btn"
           data-testid="login-submit-btn"
           disabled={ !(password.length > passwordLength
           && email.includes('@') && email.includes('.com')) }
