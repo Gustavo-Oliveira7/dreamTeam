f58d5657af612db35e6e1291246536d8c1eb0288
Gustavo-Oliveira7 <gustavo.h.oliveira7531@gmail.com>
2023-04-19 00:45:35 -0300

    criando uma pagina para o perfil

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Main from './pages/Main';
+import Profile from './pages/Profile';
 
 function Rotas() {
   return (
     <Routes>
       <Route exact path="/" element={ <Login /> } />
       <Route path="/main" element={ <Main /> } />
+      <Route path="/profile" element={ <Profile /> } />
     </Routes>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.png';
 
 function Header() {
+  const history = useNavigate();
+
   return (
     <div className="header">
       <h1>header</h1>
-      <img
-        className="iconProfile"
-        alt={ profileIcon }
-        src={ profileIcon }
-      />
+      <Link
+        to="/profile"
+        onClick={ history('/main') }
+      >
+
+        <img
+          className="iconProfile"
+          alt={ profileIcon }
+          src={ profileIcon }
+        />
+      </Link>
     </div>
   );
 }
